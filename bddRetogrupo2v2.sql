48c84c10675c3222054981312ec6f8ac854f49e8
Liher124 <90892636+Liher124@users.noreply.github.com>
2021-11-08 13:57:10 +0100

    sdac

--- bddRetogrupo2v2.sql
+++ bddRetogrupo2v2.sql
@@ -18,6 +18,24 @@ drop table if exists proyecto;
 drop table if exists gastos;
 
 /****		TABLAS		****/
+/* Definitivo sin hacer
+create table usuarios (
+	intIdUser int(10) not null auto_increment,
+	intIdDepartamento int(10) not null,
+    vchNombreUser varchar(50) unique not null,
+    vchNombre varchar(20) not null,
+    vchApellido1 varchar(20) not null,
+    vchApellido2 varchar(20),
+	vchEmail varchar(50) unique not null,
+    vchPassword varchar(255) not null,
+    vchRol varchar(20) not null,
+    dateUltimaConexion datetime,
+    vchEstado varchar(50),
+    primary key (intIdUser),
+    foreign key (intIdDepartamento) references departamento(intIdDepartamento)
+);
+*/
+
 create table empleados (
     intIdEmpleado int(10) not null auto_increment,
     intIdUser int(10) not null,
@@ -55,9 +73,9 @@ PRIMARY KEY(intidpro)
 );
 
 create table departamento(
-intidDep int,
-vchNom varchar(30),
-PRIMARY KEY(intidDep)
+intIdDepartamento int(10),
+vchNombre varchar(50),
+PRIMARY KEY(intIdDepartamento)
 );
 
 
@@ -79,7 +97,8 @@ vchpais varchar(30),
 vchciudad varchar(30),
 dateFechainicio date,
 dateFechafin date,
-intdifdias int
+intdifdias int,
+intIdUser int(10)
 );
 
 
@@ -102,7 +121,8 @@ Add constraint fk2_gastos FOREIGN KEY (intidpro) references proyecto(intidpro);
 Alter table gastos
 Add constraint fk3_gastos FOREIGN KEY (intidDep) references departamento(intidDep);
 
-
+Alter table gastos
+Add constraint fk4_gastos FOREIGN KEY (intIdUser) references empleados(intIdUser);
 /*****************************
 INSERTAR DATOS EN LAS TABLAS
 *****************************/ 
@@ -119,17 +139,25 @@ delete from proyecto;
 /*	Insertar datos tabla empleados */ 
 ALTER TABLE empleados AUTO_INCREMENT=1;
 insert into empleados (intIdUser,vchNombre,vchApellido1,vchApellido2,vchCorreo,vchDni,vchEspecialidad,intidDep,intidGasto) values 
-(1,"Liher","Ramoneda","Vicente","liher.ramoneda@maristak.net","7892667B","Desarrollador",1,1),
-(2,"Liher","Ramoneda","Vicente","liher.ramoneda@gmail.com","9138384X","Informatico",1,1);
+(1,"Liher","Ramoneda","Vicente","liher.ramoneda@maristak.net","7892667B","Desarrollo",1,1),
+(2,"Xabier","Parra","Navarro","xabi.parra@maristak.net","9138384X","Transporte",3,1),
+(3,"Jon","Herrero","","jon.herrero@gmail.com","7338384O","Abogados",2,1),
+(4,"Daniel","Alvarez","Toledo","daniel.alvarez@gmail.com","2137154I","Programacion",5,1),
+(5,"Javier","Garcia","Torrel","javier.garcia@gmail.com","9138384L","Desarrollo",1,1);
 select * from empleados;
 
 
 -- TABLA PROYECTO
+ALTER TABLE proyecto AUTO_INCREMENT=1;
 insert into proyecto (intidpro,vchNom,dateFechaInicio,dateFechafin,doublePresupuesto) VALUES (1,"Aplicacion Android",'2021/10/15','2021/12/20',30000.20);
 insert into proyecto (intidpro,vchNom,dateFechaInicio,dateFechafin,doublePresupuesto) VALUES (2,"Aplicacion Web",'2021/10/15','2021/12/20',30000.30);
+insert into proyecto (intidpro,vchNom,dateFechaInicio,dateFechafin,doublePresupuesto) VALUES (3,"Delta",'2021/10/15','2021/12/20',30000.30);
+insert into proyecto (intidpro,vchNom,dateFechaInicio,dateFechafin,doublePresupuesto) VALUES (4,"Creacion Base de datos",'2021/10/15','2021/12/20',30000.30);
+insert into proyecto (intidpro,vchNom,dateFechaInicio,dateFechafin,doublePresupuesto) VALUES (5,"Gestion Empresa",'2021/10/15','2021/12/20',30000.30);
 select * from proyecto;
 
 -- TABLA DEPARTAMENTO
+ALTER TABLE departamento AUTO_INCREMENT=1;
 insert into departamento (intidDep,vchNom) VALUES (1,"Desarrollo");
 insert into departamento (intidDep,vchNom) VALUES (2,"Abogados");
 insert into departamento (intidDep,vchNom) VALUES (3,"Transporte");
--- consultasRetov2.sql
+++ consultasRetov2.sql
@@ -1,8 +1,15 @@
 use wordpress;
 select * from usuarios;
 select * from empleados;
+select * from empleados;
 select * from gastos;
 select * from proyecto;
 
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_SAFE_UPDATES=0;	
 
-
+delete from empleados;
+delete from usuarios;
+delete from departamento;
+delete from gastos;
+delete from proyecto;
